b3aa88820c63806fa7fbde39a432ddf4c463afb0
SadShrimpy <alessionice@gmail.com>
2024-10-16 17:42:32 +0200

    Refactor to use static method calls
    Removed unnecessary instantiations of `NetworkUtility` and `JavaDoc` classes in the `main` method. Refactored method calls to use static access, enhancing code clarity and reducing unused object creation.

--- src/Main.java
+++ src/Main.java
@@ -16,8 +16,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Main {
 
     public static void main(String[] args) {
-        NetworkUtility networkUtility = new NetworkUtility();
-        JavaDoc javadoc = new JavaDoc();
         boolean fetchAllJavadocs = true;
 
         if (CacheHandler.cacheExists()) {
@@ -26,13 +24,13 @@ public class Main {
 
         if (fetchAllJavadocs) {
             Log.logWarn("Couldn't find cache file, fetching all javadocs");
-            String log = networkUtility.fetchFileFromUrl(URLS.METADATA.get(), FILES.MAVEN_METADATA_XML.get())
+            String log = NetworkUtility.fetchFileFromUrl(URLS.METADATA.get(), FILES.MAVEN_METADATA_XML.get())
                 ? "Successfully downloaded " + FILES.MAVEN_METADATA_XML.get() + " from " + URLS.METADATA.get()
                 : "Failed to download " + FILES.MAVEN_METADATA_XML.get() + " from " + URLS.METADATA.get();
             Log.auto(log);
         }
 
-        javadoc.generateAllJavadoc();
+        JavaDoc.generateAllJavadoc();
     }
 
 }
